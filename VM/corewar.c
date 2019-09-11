d87ff80b62ca7f49733844324df7809a1826e381
SachaRbs <sacharobberse7@gmail.com>
2019-09-11 14:43:57 +0200

    parsing ok vm debut

--- VM/corewar.c
+++ VM/corewar.c
@@ -1,20 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_vm.c                                          :+:      :+:    :+:   */
+/*   corewar.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/05 12:15:41 by sarobber          #+#    #+#             */
-/*   Updated: 2019/09/11 14:42:53 by sarobber         ###   ########.fr       */
+/*   Created: 2019/09/11 10:39:50 by sarobber          #+#    #+#             */
+/*   Updated: 2019/09/11 12:05:22 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/vm.h"
 
-int main(int ac, char **av)
+void	run_corewar(t_vm *vm)
 {
-    t_vm vm;
-    initialize(&vm, ac, av);
-	run_corewar(&vm);
+	t_proc *proc;
+	
+	while (proc = vm->proc && (vm->dump == -1 || vm->cycle < vm->dump))
+	{
+		while (proc)
+		{
+			
+			proc = proc->next;
+		}	
+		vm->cycle++;
+	}
 }
--- VM/init_vm.c
+++ VM/init_vm.c
@@ -6,7 +6,7 @@
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 13:56:05 by sarobber          #+#    #+#             */
-/*   Updated: 2019/09/09 18:17:48 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/09/11 10:46:55 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,14 @@ void    parsing(t_vm *vm, int ac, char **av)
 	{
 		if (av[i][0] == '-' )
 		{
-			if (av[i][1] == 'n' && !av[i][2] && ac > ++i)
+			if (av[i][1] == 'n' && !av[i][2] && ac > ++i) {
 				vm->pnum[vm->pct] = ft_atoi(av[i]); // atoi qui check si le numero de -n 'X' est bon ( < MAX_PLAYERS)
-			//else if 'a'/ 'd'
-			 //
+				vm->play_free[vm->pnum[vm->pct]] = 1;
+			}
+			else if (av[i][1] == 'd')
+				vm->dump = ft_atoi(av[i]);
 			else if (write(2, "ERROR mauvaise option\n", 23))
-				exit(1);//ERROR
+				exit(-1);
 		}
 		else if (vm->pct < MAX_PLAYERS)
 		{
@@ -46,56 +48,68 @@ void    parsing(t_vm *vm, int ac, char **av)
 			vm->pnum[vm->pct] = -1;
 		}
 		else if (write(2, "ERROR trop de champions\n", 25))
-			exit(1);//ERROR
+			exit(-1);
 	}
 }
 
- // note un octet hexa= 2 chiffres peut stocker 16^2 = 256 comme un octet en binaire avec 8 chiffres 2^8=256
-	// read(fd, (void*)&h.magic, 4); //= f383ea00f383ea00 ??
-int		read_proc(t_proc *current, int fd, char *prog, char **name) // parse le chmpion
-{ // gestion d'erreur
-	header_t h;
+int16_t reverser_16(int16_t a)
+{
+    return (((a & 0xFF00) >> 8) | ((a & 0x00FF) << 8));
+}
+int32_t reverser_32(int32_t a)
+{
+    int32_t tmp;
+    tmp = 0;
+    tmp = ((a & 0xFF000000) >> 24) | ((a & 0x00FF) << 24);
+    tmp |= ((a & 0x00FF0000) >> 8) | ((a & 0x0000FF00) << 8);
+    return (tmp);
+}
+
+int		read_proc(t_proc *current, int fd, unsigned char *prog, char **name) // parse le chmpion
+{
+	header_t *h;
+	int rd;
+	
+	if (!(h = malloc(sizeof(header_t))) && write(1, "MALLOC erreur\n", 14))
+		return(-1);
+	if ((rd = read(fd, h, sizeof(header_t))) < 0 && write(2, "impossible de lire le fichier\n", 31))
+		return (-1);
+	if (reverser_32(h->magic) != COREWAR_EXEC_MAGIC && write(2, "Nombre magique faux\n", 21))
+		return(-1);
+	if (reverser_32(h->prog_size) > CHAMP_MAX_SIZE && write (2, "size du champion trop grande\n", 30))
+		return (-1);
+	read(fd, prog, CHAMP_MAX_SIZE + 1);
+	printf("* Player %d, weighing %d bytes, \"%s\" (\"%s\") !\n", current->pnu, reverser_32(h->prog_size), h->prog_name, h->comment);
+	return(reverser_32(h->prog_size));
+}
+
+int		find_playernum(t_vm *vm)
+{
+	int min;
 	int i;
 	
-	i = 4;
-	while (i-- || !(i = 8)) //on verra
-		read(fd, (void*)&h.magic + i, 1);
-	read(fd, h.prog_name, PROG_NAME_LENGTH);
-	while ((h.magic == COREWAR_EXEC_MAGIC || !(h.prog_size = ~0)) && i--)
-		read(fd, (void*)&h.prog_size + i, 1);
-	if (h.prog_size > CHAMP_MAX_SIZE || h.magic != COREWAR_EXEC_MAGIC)
-		return(-1);
-	while (h.prog_name[++i] && (i < PROG_NAME_LENGTH || (h.prog_name[i] = 0)))
-		;
-	read(fd, h.comment, COMMENT_LENGTH);
-	read(fd, &h.magic, 4); // lire 4 octet NUULL si h.magic tester
-	read(fd, prog, CHAMP_MAX_SIZE + 1);
-	if (!((*name) = malloc(++i)))
-		return (-1); //ERROR
-	while(i--)
-		(*name)[i] = h.prog_name[i];
-	close(fd);
-	return (h.prog_size);
+	min = 10;
+	i = 0;
+	while (i++ < MAX_PLAYERS)
+		if (min > i && vm->play_free[i] == 0)
+			min = i;
+	vm->play_free[min] = 1;
+	return (min);
 }
 
 void	load_proc(t_vm *vm, int fd, t_proc *current, int pn)  //rentre le champion dans la memoire
 {
-	char prog[CHAMP_MAX_SIZE];
+	unsigned char prog[CHAMP_MAX_SIZE];
 	int i;
 	int orig;
 
 	if ((vm->sizes[pn] = read_proc(current, fd, prog, &vm->names[pn])) == -1)
-		exit(1); //ERROR
+		exit(-1);
 	i = MEM_SIZE / vm->pct + ((int)current->pc >= MEM_SIZE / vm->pct);// verifier
 	while (i--)
 		vm->mem[current->pc - i] = i >= vm->sizes[pn] ? 0 : prog[i];
-	if ((orig = vm->pnum[pn]) == -1) // si pas de flag -n
-		vm->pnum[pn] = pn ? vm->pnum[pn - 1] : 1; //playeur precedent plus 1 si pas le premier, sinon Pl1
-	i = pn;
-	while (i--) {
-		if (vm->pnum[pn] < 0 || vm->pnum[pn] == orig)
-			;//continuer ici
-	}
+	bzero(current->reg, REG_NUMBER * REG_SIZE);
+	current->reg[1] = -current->pnu;
 }
 
 
@@ -104,11 +118,17 @@ void	check_proc(t_vm *vm, t_proc *current, int pn)
 	int fd;
 
 	pushfront_proc(&vm->proc, current);
-	if ((fd = open(vm->names[pn], O_RDONLY)) == -1){
+	if ((fd = open(vm->names[pn], O_RDONLY)) == -1)
+	{
 		printf("fail to open\nfd = %d", fd);
-		exit(1);
+		exit(-1);
 	}
+	current->carry = false;
 	current->pc = MEM_SIZE - 1 - (pn * MEM_SIZE / vm->pct); //pc = emplacement dans la memoire du curseur du processus
+	if (vm->pnum[pn] == -1)
+		current->pnu = find_playernum(vm);
+	else
+		current->pnu = vm->pnum[pn];
 	load_proc(vm, fd, current, pn);
 }
 
@@ -118,15 +138,28 @@ int     initialize(t_vm *vm, int ac, char **av)
 	t_proc *proc;
 	
 	i = -1;
+	int j = -1;
+	bzero(vm->play_free, MAX_PLAYERS);
+	vm->dump = -1;
 	vm->pct = 0;
+	vm->cycle_to_die = CYCLE_TO_DIE;
+	vm->cycle = 0;
+	vm->check = 0;
     if (ac > 1)
         parsing(vm, ac, av);
-	else if (write (2 ,"ERROR aucun champions fournis\n", 31))
-		exit(1);//ERROR
+	else if (write (2 ,"USAGE\n", 31))
+		exit(-1);//ERROR
+	if (!vm->pct && write(2, "Error: please provide champions\n", 32))
+		exit(-1);
+	printf("Introducing contestants...\n");
 	while (++i < vm->pct)
 	{
 		proc = malloc(sizeof(t_proc));
 		check_proc(vm, proc, i);
 	}
+	// i = -1;
+	// while(++i <MEM_SIZE){              //Print memory
+	// 	printf("%x ", vm->mem[i]);
+	// }
 	return (0);
 }
