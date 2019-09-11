6b527bd508dfcd59ff8f028473d17b9f97a9e3b9
SachaRbs <sacharobberse7@gmail.com>
2019-09-11 14:48:10 +0200

    corewar

--- VM/corewar.c
+++ VM/corewar.c
@@ -6,7 +6,7 @@
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 10:39:50 by sarobber          #+#    #+#             */
-/*   Updated: 2019/09/11 12:05:22 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/09/11 14:47:54 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,14 @@ void	run_corewar(t_vm *vm)
 	{
 		while (proc)
 		{
-			
+			if (vm->cycle == proc->cycle_to_do)
+			{
+				//cycle necessaire atteint et et op code + arg enregistrer
+			}
+			else if (proc->cycle_live < vm->cycle)
+			{
+				//parser l'instruction
+			}
 			proc = proc->next;
 		}	
 		vm->cycle++;
