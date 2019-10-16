ed67da2f318e975ea42c4ba02d3a4e2d46406b13
SachaRbs <sacharobberse7@gmail.com>
2019-10-16 18:40:24 +0200

    ft_zjmp

--- vm/src/ft_zjmp.c
+++ vm/src/ft_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 20:32:29 by crfernan          #+#    #+#             */
-/*   Updated: 2019/10/16 13:21:02 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/10/16 18:32:00 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,7 @@
 
 void	*ft_zjmp(t_vm *vm, t_proc *proc)
 {
-	int i;
-
-	i = 0;
-    (void)vm;
-	printf("PLAYER No : %d\n", proc->pnu);
-	printf("FUNCTION  : ft_zjmp\n");
-	printf("action = %s\n", op_tab[proc->action].name);
-	while (i < 4)
-	{
-		printf("arg_v[%d] = %d type = ", i, proc->arg_v[i]);
-		printf("arg_t[%d] = %d\n", i, proc->arg_t[i]);
-		proc->arg_v[i] = 0;
-		proc->arg_t[i] = 0;
-		i++;
-	}
-	printf("\n\n");
-    return (NULL);
+	if (proc->carry == 1)
+		proc->pc = proc->arg_a[0] + (proc->arg_v[0] % IDX_MOD);
+	proc->carry = 0; //Not sure of this
 }
