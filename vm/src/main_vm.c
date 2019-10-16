19cd8edf7a527d7e3a594f7dcbcbbff9edae8662
SachaRbs <sacharobberse7@gmail.com>
2019-10-16 17:14:08 +0200

    change prototype initialize in corewar.h and the malloc of vm in the main

--- vm/src/main_vm.c
+++ vm/src/main_vm.c
@@ -6,7 +6,7 @@
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 12:15:41 by sarobber          #+#    #+#             */
-/*   Updated: 2019/10/16 15:29:06 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/10/16 17:12:19 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int ac, char **av)
 {
 	t_vm	*vm;
 
-	if ((vm = ft_memalloc(sizeof(t_vm*))) == NULL)
+	if ((vm = (t_vm*)ft_memalloc(sizeof(t_vm))) == NULL)
 		ft_exit(vm, ERROR_MALLOC);
 	initialize(vm, ac, av);
 	run_corewar(vm);
