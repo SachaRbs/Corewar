56c812db71147624a986270272ffae3cc276def7
SachaRbs <sacharobberse7@gmail.com>
2019-10-16 18:45:12 +0200

    ft_and or xor

--- vm/src/ft_and.c
+++ vm/src/ft_and.c
@@ -6,7 +6,7 @@
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 20:29:12 by crfernan          #+#    #+#             */
-/*   Updated: 2019/10/16 13:21:02 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/10/16 18:44:47 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,36 @@
 #include "error.h"
 #include "op.h"
 
+int		argument(t_vm *vm, t_proc *proc, int arg)
+{
+	if (proc->arg_t[arg] == REG_CODE)
+		return(proc->reg[proc->arg_v[arg]]);
+	if (proc->arg_t[arg] == DIR_CODE)
+		return(proc->arg_v[arg]);
+	return(big_endian(vm->mem[proc->arg_a[arg]
+		+ (proc->arg_v[arg] % IDX_MOD)], 4));
+}
+
 void	*ft_and(t_vm *vm, t_proc *proc)
 {
-	int num[2];
-	int i;
-
-	i = -1;
-	while(++i < 2)
-	{
-		if (proc->arg_t[i] = T_REG)
-			num[i] = proc->arg_v[i];
-		else if (proc->arg_t[i] = T_DIR)
-			;// ??
-		else if (proc->arg_t[i] = T_IND)
-			num[i] = proc->pc + (proc->arg_v[i] % IDX_MOD);
-	}
-	proc->reg[proc->arg_v[2]] = num[0] & num[1];
+	proc->reg[proc->arg_v[2]] = argument(vm, proc, 0) & argument(vm, proc, 1);
+	return (NULL);
 }
+
+// void	*ft_and(t_vm *vm, t_proc *proc)
+// {
+// 	int num[2];
+// 	int i;
+
+// 	i = -1;
+// 	while(++i < 2)
+// 	{
+// 		if (proc->arg_t[i] = T_REG)
+// 			num[i] = proc->arg_v[i];
+// 		else if (proc->arg_t[i] = T_DIR)
+// 			;// ??
+// 		else if (proc->arg_t[i] = T_IND)
+// 			num[i] = proc->pc + (proc->arg_v[i] % IDX_MOD);
+// 	}
+// 	proc->reg[proc->arg_v[2]] = num[0] & num[1];
+// }
--- vm/src/ft_xor.c
+++ vm/src/ft_xor.c
@@ -6,7 +6,7 @@
 /*   By: sarobber <sarobber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 20:32:14 by crfernan          #+#    #+#             */
-/*   Updated: 2019/10/16 13:21:02 by sarobber         ###   ########.fr       */
+/*   Updated: 2019/10/16 18:44:43 by sarobber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 #include "error.h"
 #include "op.h"
 
+int		argument(t_vm *vm, t_proc *proc, int arg)
+{
+	if (proc->arg_t[arg] == REG_CODE)
+		return(proc->reg[proc->arg_v[arg]]);
+	if (proc->arg_t[arg] == DIR_CODE)
+		return(proc->arg_v[arg]);
+	return(big_endian(vm->mem[proc->arg_a[arg]
+		+ (proc->arg_v[arg] % IDX_MOD)], 4));
+}
+
 void	*ft_xor(t_vm *vm, t_proc *proc)
 {
-	int i;
-
-	i = 0;
-    (void)vm;
-	printf("PLAYER No : %d\n", proc->pnu);
-	printf("FUNCTION  : ft_xor\n");
-	printf("action = %s\n", op_tab[proc->action].name);
-	while (i < 4)
-	{
-		printf("arg_v[%d] = %d\n", i, proc->arg_v[i]);
-		proc->arg_v[i] = 0;
-		i++;
-	}
-	printf("\n\n");
-    return (NULL);
+	proc->reg[proc->arg_v[2]] = argument(vm, proc, 0) ^ argument(vm, proc, 1);
+	return (NULL);
 }
