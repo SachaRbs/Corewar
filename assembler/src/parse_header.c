b474d5b17ca512ceeec2bb6912de44874ed535a5
Yoann RIBEIRO <yoribeir@student.42.fr>
2019-10-29 16:52:39 +0100

    multi line

--- assembler/src/parse_header.c
+++ assembler/src/parse_header.c
@@ -12,37 +12,55 @@
 
 #include "../includes/asm.h"
 
+char	*parse_multiline(t_asm *p, char *line)
+{
+	size_t	size;
+	char	*buffer;
+	char	*str;
+	char	*end;
+
+	str = ft_strdup(line + p->col);
+	while (ft_readline(p->fd, &p->str, &buffer) > 0 && (!(end = ft_strchr(buffer, '"'))))
+	{
+		str = ft_strjoin(str, buffer);
+		p->row++;
+	}
+	size = ft_strlen(str);
+	str = ft_strjoin(str, buffer);
+	p->col = 0;
+	while (buffer[p->col] && buffer[p->col] != '\n')
+		p->col++;
+	if (buffer[p->col] == '\n')
+	{
+		p->col++;
+		p->row++;
+	}
+	return (ft_strsub(str, 0, p->col + size - 2));
+}
+
 void	parse_champion(t_asm *p, char *line)
 {
 	size_t		start;
-	char		*end;
-	char		*str;
 
 	start = 0;
-	str = NULL;
-	end = NULL;
     while (is_whitespace(line[p->col]) && line[p->col])
         p->col++;
     if (line[p->col] == '"')
     {
         p->col++;
 		start = p->col;
-		printf("1%s\n", line);
-		str = ft_strdup(line);
-		while (ft_readline(p->fd, &p->str, &line) > 0 && !(end = ft_strchr((str), '"')))
+		if (ft_strchr(line + p->col, '"'))
 		{
-			printf("2%s\n", line);
-			str = ft_strjoin(str, line);
+			while (line[p->col] && line[p->col] != '"')
+				p->col++;
+			p->champ = ft_strsub(line + start, 0, p->col - start);
 		}
-		str = ft_strjoin(str, line);
-		printf("[%s]\n", str);
+		else
+			p->champ = parse_multiline(p, line);
     }
 	else
 		ft_lexerror(p);
-	printf("%ld\n", (end - (str + start)));
-    // p->champ = ft_strsub(str + start, 0, (end - (str + start)));
-	printf(GRN"%s\n"RESET, p->champ);
-    // p->champ = ft_strsub(line + start, 0, p->col - start);
+	printf(GRN"[%s]\n"RESET, p->champ);
     if (p->champ && ft_strlen(p->champ) > PROG_NAME_LENGTH)
         ft_error("CHAMPION NAME TOO LONG");
 }
@@ -58,11 +76,16 @@ void	parse_comment(t_asm *p, char *line)
     {
         p->col++;
 		start = p->col;
-		// NEEDS TO READ LINE UNTIL NEXT QUOTE
-		while (line[p->col] && line[p->col] != '"')
-			p->col++;
+		if (ft_strchr(line + p->col, '"'))
+		{
+			while (line[p->col] && line[p->col] != '"')
+				p->col++;
+			p->comment = ft_strsub(line + start, 0, p->col - start);
+		}
+		else
+			p->comment = parse_multiline(p, line);
     }
-    p->comment = ft_strsub(line + start, 0, p->col - start);
+	printf(GRN"[%s]\n"RESET, p->comment);
     if (ft_strlen(p->comment) > COMMENT_LENGTH)
         ft_error("CHAMPION COMMENT TOO LONG");
 }
