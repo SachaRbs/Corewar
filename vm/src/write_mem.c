a3e5c864a946c32c195287bb19f6f36cb9ed611b
Cristina FERNANDEZ <crfernan@e1r1p1.42.fr>
2019-11-06 14:09:14 +0100

    norm write_mem.c

--- vm/src/write_mem.c
+++ vm/src/write_mem.c
@@ -6,33 +6,21 @@
 /*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 17:01:36 by crfernan          #+#    #+#             */
-/*   Updated: 2019/11/05 18:52:40 by crfernan         ###   ########.fr       */
+/*   Updated: 2019/11/06 14:06:03 by crfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 #include "error.h"
 
-void	writing_mem(t_vm *vm, int pc, int bytes, int value)
+void	write_negative(t_vm *vm, int pc, int bytes, int value)
 {
-	int i;
-	int sign;
-	int end;
 	int div;
 	int hex;
-	int address;
+	int	i;
 
-	sign = 1;
-	bytes *= 2;
-	if (pc < 0)
-		pc = MEM_SIZE + pc;
-	if (value < 0)
-	{
-		sign = -1;
-		value *= -1;
-	}
 	i = 0;
-	while(i < bytes)
+	while (i < bytes)
 	{
 		div = ft_power(16, (bytes - 1 - i));
 		hex = 16 * (value / div);
@@ -41,13 +29,40 @@ void	writing_mem(t_vm *vm, int pc, int bytes, int value)
 		div = ft_power(16, (bytes - 1 - i));
 		hex += (value / div);
 		i++;
-		address = mod_address(pc + (i - 1) / 2);
-		if (sign == 1)
-			vm->mem[address] = hex;
-		else if ((end = value % div))
-			vm->mem[address] = 255 - hex;
+		if (div != 1)
+			vm->mem[mod_address(pc + (i - 1) / 2)] = 255 - hex;
 		else
-			vm->mem[address] = 256 - hex;
+			vm->mem[mod_address(pc + (i - 1) / 2)] = 256 - hex;
 		value %= div;
 	}
+}
+
+void	write_positive(t_vm *vm, int pc, int bytes, int value)
+{
+	int div;
+	int hex;
+	int	i;
+
+	i = 0;
+	while (i < bytes)
+	{
+		div = ft_power(16, (bytes - 1 - i));
+		hex = 16 * (value / div);
+		i++;
+		value %= div;
+		div = ft_power(16, (bytes - 1 - i));
+		hex += (value / div);
+		i++;
+		vm->mem[mod_address(pc + (i - 1) / 2)] = hex;
+		value %= div;
+	}
+}
+
+void	writing_mem(t_vm *vm, int pc, int bytes, int value)
+{
+	pc = mod_address(pc);
+	if (value < 0)
+		write_negative(vm, pc, bytes * 2, value * -1);
+	else
+		write_positive(vm, pc, bytes * 2, value);
 }
